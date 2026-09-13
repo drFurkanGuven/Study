bb6cd102e65b073c40f3fe73247352f7d5b35c8e
furkan <furkan@localhost>
2026-09-13 18:54:53 +0300

    tilki UI v4: keskin piksel assetler + butun site tek dil, buyuyen bitki

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,3 +1,3 @@
-const C='odak-v1';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['/static/style.css','/static/app.js','/static/icon.svg'])).then(()=>self.skipWaiting()));});
+const C='odak-v2';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['/static/style.css','/static/app.js','/static/fox.svg','/static/scenery.svg','/static/icon.svg'])).then(()=>self.skipWaiting()));});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
